0fc4c92d5063a467c51a4145b1269cc6a7c55a76
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-01-27 14:00:46 +0600

    initial methods implemented with dummy datas

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-
-dotenv.config({path : './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Show all bootcamps'
+  })
 })
 
-app.listen(PORT , console.log(`server running at ${process.env.PORT}`))
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(201).json({
+    success: true,
+    message: 'create new bootcamp'
+  })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  const itemId = req.params.id;
+  console.log(itemId);
+  res.status(200).json({
+    success: true,
+    message: `Bootcamp update for ${itemId}`
+  })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  const itemId = req.params.id;
+  console.log(itemId);
+  res.status(200).json({
+    success: true,
+    message: `Bootcamp delete for ${itemId}`
+  })
+})
+
+app.listen(PORT, console.log(`server running at ${process.env.PORT}`))
