7ef08fbd7531f8e678b94eef1a0fccc3ed4ff0f4
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-03-02 11:27:21 +0600

    server  connected to local mongodb database

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,28 +1,19 @@
-var express = require('express');
-const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
+var express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
 var router = express.Router();
 
-// router.get('/', getBootcamps);
-
-// router.post('/', createBootcamp);
-
-// router.get('/:id', getBootcamp);
-
-// router.put('/:id', updateBootcamp);
-
-// router.delete('/:id', deleteBootcamp);
-
+router.route("/").get(getBootcamps).post(createBootcamp);
 
 router
-    .route('/')
-    .get(getBootcamps)
-    .post(createBootcamp)
-
-router
-    .route('/:id')
-    .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp)
-
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bootcamps = require('./routes/bootcamps.js');
+const express = require("express");
+const dotenv = require("dotenv");
+const bootcamps = require("./routes/bootcamps.js");
+const connectDB = require("./config/db");
 
-// mounting the routes 
-dotenv.config({ path: './config/config.env' });
+// mounting the routes
+dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use('/api/v1/bootcamps', bootcamps);
+// connect to database
+connectDB();
 
-app.listen(PORT, console.log(`server running at ${process.env.PORT}`))
+// chaining to the
+app.use("/api/v1/bootcamps", bootcamps);
+
+const server = app.listen(
+  PORT,
+  console.log(`server running at ${process.env.PORT}`)
+);
+
+//handle unhandled promise rejection
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`);
+  // close and exit
+  server.close(() => process.exit(1));
+});
